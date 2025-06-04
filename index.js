7e498696578e05726af2916cb1691e49c429bd2f
Mohammad Adnan <mohmmadadnan930@gmail.com>
2025-06-04 09:36:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
 
-// var randomNumber1=Math.floor(Math.random()*6)+1;
-
-// var randomdiceimg1="./images/dice"+randomNumber1+".png";
-
-// document.querySelectorAll("img")[0].setAttribute("src",randomdiceimg1);
-
-
-
-// var randomNumber2=Math.floor(Math.random()*6)+1;
-// var randomdiceimg2="./images/dice"+randomNumber2+".png";
-// document.querySelectorAll("img")[1].setAttribute("src",randomdiceimg2);
-
-// if(randomNumber1==randomNumber2){
-//     document.querySelector("h1").innerHTML="it's a Draw";
-// }else if(randomNumber1<randomNumber2){
-//     document.querySelector("h1").innerHTML="Player 2 wins";
-    
-// }else if(randomNumber1>randomNumber2){
-//     document.querySelector("h1").innerHTML="Player 1 wins";
-// }
-//For first dice
 var randomnumber1=Math.floor(Math.random()*6)+1;
 var randomimg="./images/dice"+randomnumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomimg);
